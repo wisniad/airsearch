c1810dd0aafd3310bdb4c6bf0aeb9ff463459328
wisniad <wisniad@gmail.com>
2018-07-18 19:10:35 +0200

    Working search created

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,11 +32,11 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json','scss'],
+    extensions: ['.js', '.vue', '.json','sass'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      styles: resolve('src/assets/scss'),
+      styles: resolve('src/assets/sass'),
     }
   },
   module: {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,61 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+<template lang="pug">
+.main
+  .center-flex
+    h1 Airport EC261
+  .center-flex
+    input(
+      v-model="search",
+      type="text",
+      placeholder="Find airport"
+      )
+  .center-flex
+    table
+      thead
+        tr
+        th IATA
+        th EC261
+        th Name
+        th Country
+        th City
+      tbody
+        tr(
+          v-for='airport in airports',
+          :class = "airport['ec261'] ? 'bg-true' : 'bg-false' ",
+          )
+          th {{ airport['iata'] }}
+          th {{ airport['ec261'] }}
+          th {{ airport['name'] }}
+          th {{ airport['country'] }}
+          th {{ airport['city'] }}
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
+      search: '',
+      airports: [],
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  watch: {
+    search: function (val) {
+      if (val.length > 0) {
+        axios.get(`https://app.airhelp.com/api/airports?with_metropolitan_areas=false&q= ${val}`)
+          .then((response) => {
+            this.airports = []
+            response.data.airports.map(airport => {
+              this.airports.push(airport)
+            })
+          })
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="sass" src="../assets/sass/main.sass">
 </style>
